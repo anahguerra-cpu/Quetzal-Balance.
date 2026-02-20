531d681072748e6884217ceaea53310b9c12d461
anahguerra-cpu <anahguerra@gmail.com>
2026-02-20 17:46:54 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+const CACHE_NAME = "quetzal-balance-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./logo-192.png",
+  "./logo-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
